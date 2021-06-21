d4b776ea85d3648f9d57649901a4abdc56dff1ea
User <zomlesh@yandex.ru>
2021-06-21 13:49:40 +0300

    Заголовки для CORS

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,12 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://zomlesh.nomoredomains.club");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
