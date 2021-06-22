848febecf4276ffb601c87f123c876448316b632
Sasha Leshiy <zomlesh@yandex.ru>
2021-06-22 21:38:03 +0300

    logger

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -10,8 +10,7 @@ const requestLogger = expressWinston.logger({
 
 const errorLogger = expressWinston.errorLogger({
     transports: [
-        // new winston.transports.File({ filename: 'error.log' }),
-        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'error.log' }),
     ],
     format: winston.format.json(),
 });
