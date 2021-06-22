f35541933b9985ac3a7bb04f788dd4431a6e8379
User <zomlesh@yandex.ru>
2021-06-22 16:23:35 +0300

    Логирование

--- backend/middlewares/Logger.js
+++ backend/middlewares/Logger.js
@@ -4,7 +4,6 @@ const expressWinston = require('express-winston');
 const requestLogger = expressWinston.logger({
     transports: [
         new winston.transports.File({ filename: 'request.log' }),
-        new winston.transports.Console(),
     ],
     format: winston.format.json(),
 });
@@ -12,7 +11,6 @@ const requestLogger = expressWinston.logger({
 const errorLogger = expressWinston.errorLogger({
     transports: [
         new winston.transports.File({ filename: 'error.log' }),
-        new winston.transports.Console(),
     ],
     format: winston.format.json(),
 });
