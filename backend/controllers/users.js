f77530862c9acc970cfcea0d07e3c45e36e45c47
Sasha Leshiy <zomlesh@yandex.ru>
2021-06-24 20:39:12 +0300

    Правка роутеров

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -124,6 +124,7 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true }).send({ token });
     })
     .catch((err) => {
+      console.log(err.name);
       next(err);
     });
 };
