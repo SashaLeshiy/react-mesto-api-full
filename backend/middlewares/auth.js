0089d3a0ef1782dfd8f8f37a821ac9df92bbf12a
Sasha Leshiy <zomlesh@yandex.ru>
2021-06-26 14:47:36 +0300

    Настройка роутеров

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -8,14 +8,14 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    return res.status(401).send({ message: 'Необходима авторизация1' });
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    return res.status(401).send({ message: 'Необходима авторизация2' });
   }
   req.user = payload;
   next();
