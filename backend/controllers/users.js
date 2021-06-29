1271f366e0c82991c424eb85ccd43c72e771c458
Sasha Leshiy <zomlesh@yandex.ru>
2021-06-29 15:33:38 +0300

    Корректировка фронта и бэкенда

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -23,6 +23,7 @@ module.exports.getUser = (req, res, next) => {
         err.statusCode = 404;
         next(err);
       } else {
+        console.log(user);
         res.send(user);
       }
     })
