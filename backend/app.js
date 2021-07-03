4a23ad4fcd8dcfa63143e62e41b127dbe2af70e6
Sasha Leshiy <zomlesh@yandex.ru>
2021-07-03 15:59:17 +0300

    Правка app

--- backend/app.js
+++ backend/app.js
@@ -66,27 +66,6 @@ app.use((err, req, res, next) => {
   const { message } = err;
   res.status(status).json({ message: message || 'Произошла ошибка на сервере' });
   return next();
-
-  // if (err.name === 'CastError') {
-  //   return res.status(400).send({ message: 'Некорректный _id' });
-  // }
-  // if (err.name === 'ValidationError' || err.statusCode === 400) {
-  //   return res.status(400).send({ message: 'Отправлены неверные данные' });
-  // }
-  // if (err.statusCode === 404) {
-  //   return res.status(404).send({ message: 'Не найдено' });
-  // }
-  // if (err.name === 'MongoError' && err.code === 11000) {
-  //   return res.status(409).send({ message: 'Данный email зарегистрирован' });
-  // }
-  // if (err.statusCode === 401 || err.statusCode === 420) {
-  //   return res.status(401).send({ message: 'Неверно указаны почта или пароль' });
-  // }
-  // if (err.statusCode === 403) {
-  //   return res.status(403).send({ message: 'Нет доступа' });
-  // }
-  // next();
-  // return res.status(500).send({ message: 'На сервере произошла ошибка' });
 });
 
 app.listen(PORT, () => {
