9359b786f2a5e426c0d8a479e7f5066619fd6d42
Sasha Leshiy <zomlesh@yandex.ru>
2021-07-04 20:00:50 +0300

    Правки после второго ревью

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@ app.get('/crash-test', () => {
 });
 
 app.use(requestLogger);
-app.post(`${API_PATH}/signin`, celebrate({
+app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
